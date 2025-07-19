99f88c2549395bd6c4efe065efba0a9325be51d2
depstor <depstor90@gmail.com>
2025-07-19 14:25:57 +0300

    change-color-path_fill-v1

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,26 @@
 import "/src/sass/style.scss";
 import 'purecss';
 
+const nav_items = document.querySelectorAll('.nav__item');
+const nav_svg = document.querySelectorAll('.nav__item-svg');
+
+
+const mobileNavigation = items => {
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+
+            items.forEach(el => {
+                el.classList.remove('active-dark')
+                const paths = el.querySelectorAll('path')
+                paths.forEach(p => p.setAttribute('fill', 'none'))
+            });
+
+            item.classList.add('active-dark')
+
+            const activePaths = item.querySelectorAll('path')
+            activePaths.forEach(p => p.setAttribute('fill', 'white'))
+        })
+    })
+}
+
+mobileNavigation(nav_items);
